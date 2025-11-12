471278db9d2e9f9f93aa295a3e1e7ff61e4ad18c
tohirjon <tohirjon>
2025-11-12 16:36:59 +0500

    scsdfcds

--- src/api/api.js
+++ src/api/api.js
@@ -2,25 +2,25 @@ import axios from "axios";
 
 const API_URL = "https://jsonplaceholder.typicode.com/photos";
 
-// 🔹 GET
+//  GET
 export const getAlbums = async (limit = 10) => {
   const res = await axios.get(`${API_URL}?_limit=${limit}`);
   return res.data;
 };
 
-// 🔹 POST
+//  POST
 export const createAlbum = async (newAlbum) => {
   const res = await axios.post(API_URL, newAlbum);
   return res.data;
 };
 
-// 🔹 PUT (update)
+//  PUT (update)
 export const updateAlbum = async (id, updatedAlbum) => {
   const res = await axios.put(`${API_URL}/${id}`, updatedAlbum);
   return res.data;
 };
 
-// 🔹 DELETE
+//  DELETE
 export const deleteAlbum = async (id) => {
   await axios.delete(`${API_URL}/${id}`);
 };
